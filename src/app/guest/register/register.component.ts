70d13a7d09fc85738126e7282778e3094064c7b5
Yunus.Ozturk <yunus.ozturk@anadoluefes.com>
2023-03-24 02:01:08 +0300

    register controller method added

--- src/app/guest/register/register.component.ts
+++ src/app/guest/register/register.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { faUserCircle } from '@fortawesome/free-regular-svg-icons';
+import { User } from 'src/app/models/user.model';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+
+
+  user: User = new User;
+  faUser = faUserCircle;
+  errorMessage: string = "";
+
+
+  constructor(private authenticationService: AuthenticationService, private router: Router){  }
+  
+
+
+
+  ngOnInit(): void {
+    if(this.authenticationService.currentUserValue?.id){
+
+      this.router.navigate(['/login']);
+    }
+    
+  }
+
+
+  register() {
+    this.authenticationService.register(this.user).subscribe(data => {
+      this.router.navigate(['/login']);
+    }, err => {
+      if (err?.status === 409) {
+        this.errorMessage = 'Username already exist.';
+      } else {
+        this.errorMessage = 'Unexpected error occurred. Error is: ' + err?.errorMessage;
+        console.log(err);
+      }
+    })
+  }
+
+
 
 }
