73ca8ddefed2f8bb3e5915b09030d1580eab36ef
Yunus.Ozturk <yunus.ozturk@anadoluefes.com>
2023-03-24 12:29:57 +0300

    login controller created

--- src/app/guest/login/login.component.ts
+++ src/app/guest/login/login.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { faUserCircle } from '@fortawesome/free-regular-svg-icons';
+import { User } from 'src/app/models/user.model';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  user: User = new User;
+  faUser = faUserCircle;
+  errorMessage: string = "";
+
+  constructor(private authenticationService: AuthenticationService, private router: Router){
+
+    
+  }
+  
+  ngOnInit(): void {
+    if (this.authenticationService.currentUserValue?.id) {
+      this.router.navigate(['/profile']);
+      return;
+    }
+  }
+
+  login() {
+    this.authenticationService.login(this.user).subscribe(data=>{
+      this.router.navigate(['/profile']);
+
+    },err=> {
+      this.errorMessage = 'Username or password is incorrect.'
+      console.log(err);
+    });
+    
+  }
+
+ 
 
 }
