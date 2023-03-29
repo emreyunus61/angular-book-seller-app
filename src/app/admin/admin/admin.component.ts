385d0a4e9860e0463e0d65bc8b2a869746b84524
Yunus.Ozturk <yunus.ozturk@anadoluefes.com>
2023-03-30 01:56:23 +0300

    Creating the admin page

--- src/app/admin/admin/admin.component.ts
+++ src/app/admin/admin/admin.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Book } from 'src/app/models/book.model';
+import { BookService } from 'src/app/services/book.service';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
-export class AdminComponent {
+export class AdminComponent  implements OnInit{
+
+  bookList: Array<Book> =[];
+
+  constructor(private bookService: BookService){}
+
+
+  ngOnInit(): void {
+    this.bookService.getAllBooks().subscribe(data =>{ 
+        this.bookList=data;
+    });
+   
+  }
 
 }
