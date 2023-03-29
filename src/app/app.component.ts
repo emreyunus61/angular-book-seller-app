2aa26be33e61ee09bec297cfcfff48e496a629ad
Yunus.Ozturk <yunus.ozturk@anadoluefes.com>
2023-03-29 03:17:12 +0300

    Navigasyon bar created & authentication guard add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {User} from "./models/user.model";
+import {AuthenticationService} from "./services/authentication.service";
+import {Router} from "@angular/router";
+import {Role} from "./models/role.enum";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-book-seller-app';
+  title = 'angular-book-seller';
+
+  currentUser: User = new User;
+
+  constructor(private authenticationService: AuthenticationService, private router: Router) {
+    this.authenticationService.currentUser.subscribe(data => {
+      this.currentUser = data;
+    });
+  }
+
+  isAdmin() {
+    return this.currentUser?.role === Role.ADMİN;
+  }
+
+  logOut() {
+    this.authenticationService.logOut();
+    this.router.navigate(['/login']);
+  }
 }
