7cf40107c2c3febc4eba34799971a518dac1cfa0
Yunus.Ozturk <yunus.ozturk@anadoluefes.com>
2023-04-04 01:04:44 +0300

    implementation of home page

--- src/app/guest/home/home.component.ts
+++ src/app/guest/home/home.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Book} from "../../models/book.model";
+import {faBook} from "@fortawesome/free-solid-svg-icons";
+import {AuthenticationService} from "../../services/authentication.service";
+import {BookService} from "../../services/book.service";
+import {PurchaseService} from "../../services/purchase.service";
+import {Purchase} from "../../models/purchase.model";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  bookList: Array<Book> = [];
+  faBook = faBook;
+  errorMessage: string = "";
+  infoMessage: string = "";
+
+  
+  constructor(private authenticationService: AuthenticationService,
+    private bookService: BookService,
+    private purchaseService: PurchaseService) { }
+
+  ngOnInit(): void {
+
+    this.bookService.getAllBooks().subscribe(data =>{
+      this.bookList=data;
+    });
+    
+  }
+
+
+  purchase(book: Book){
+
+
+    if(!this.authenticationService.currentUserValue?.id){
+      this.errorMessage="You should log in to buy a book";
+      return;
+    }
+    
+    const purchase = new Purchase(this.authenticationService.currentUserValue.id,book.id,book.price);
+
+    this.purchaseService.savePurchase(purchase).subscribe(data =>{
+      this.infoMessage="Mission is complated";
+    },err => {
+      this.errorMessage = "Unexpected error occurred";
+      console.log(err);
+    });
+  }
 }
