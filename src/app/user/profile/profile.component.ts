f2899d1aac1a93b497dcd1da8fd1e6be855c6a11
Yunus.Ozturk <yunus.ozturk@anadoluefes.com>
2023-04-04 01:19:54 +0300

    implementation of profil page

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PurchaseItem } from 'src/app/models/purchase-item.model';
+import { Purchase } from 'src/app/models/purchase.model';
+import { PurchaseService } from 'src/app/services/purchase.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+
+
+  purchaseItemList : Array<PurchaseItem> = [];
+
+
+  constructor(private purchaseService: PurchaseService){}
+
+  ngOnInit(): void {
+    this.purchaseService.getAllPurchaseItems().subscribe(data => {
+      this.purchaseItemList=data;
+    });
+  }
 
 }
